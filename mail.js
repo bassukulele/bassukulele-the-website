db69d3aeada6cc80b8af15e9af8cc354fefc31e5
bassukulele <131406059+bassukulele@users.noreply.github.com>
2023-04-20 22:14:22 +0100

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,11 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyDwdYe1Ua8tU5hpZl5BreNlofyDUPyKwnY",
+    authDomain: "bassukulele-the-website.firebaseapp.com",
+    projectId: "bassukulele-the-website",
+    storageBucket: "bassukulele-the-website.appspot.com",
+    messagingSenderId: "510871538126",
+    appId: "1:510871538126:web:02780ffd63881cb59cc39f",
+    measurementId: "G-L690YFNR3H"
+};
+
+firebase.initializeApp(firebaseConfig);
